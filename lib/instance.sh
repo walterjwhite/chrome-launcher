d578d5c061234e08a28eb5814fd958d07ffe809b
Walter White <WalterWhite@walterjwhite.com>
2022-09-26 00:00:00 -0400

    merged

--- lib/instance.sh
+++ lib/instance.sh
@@ -8,28 +8,23 @@ _new_instance() {
 	fi
 
 	_info 'preparing instance'
-	_INSTANCE_DIRECTORY=$($_CONF_RUN_AS_CMD mktemp -d -t chrome.$_SESSION_NAME)
+	_INSTANCE_DIRECTORY=$(mktemp -d -t chrome.$_SESSION_NAME)
 
 	_add_option "user-data-dir" $_INSTANCE_DIRECTORY
 
 	# prepare instance
-	$_CONF_RUN_AS_CMD mkdir -p ${_INSTANCE_DIRECTORY}/Default
+	mkdir -p ${_INSTANCE_DIRECTORY}/Default
 
 	if [ ! -e $_CONFIGURATION_DIRECTORY/Default/Preferences ]; then
 		_exit_with_error "$_CONFIGURATION_DIRECTORY/Default/Preferences does not exist" 1
 	fi
 
-	$_SUDO_COMMAND cp -R $_CONFIGURATION_DIRECTORY/Default/Preferences "$_INSTANCE_DIRECTORY/Default/"
-	$_SUDO_COMMAND cp -R $_CONFIGURATION_DIRECTORY/Default/Extensions "$_INSTANCE_DIRECTORY/Default/" 2>/dev/null
-
-	if [ -n "$_RUN_AS_USER" ]; then
-		$_SUDO_COMMAND chown -R $_RUN_AS_USER "$_INSTANCE_DIRECTORY/Default/Preferences"
-		$_SUDO_COMMAND chown -R $_RUN_AS_USER "$_INSTANCE_DIRECTORY/Default/Extensions" 2>/dev/null
-	fi
+	cp -R $_CONFIGURATION_DIRECTORY/Default/Preferences "$_INSTANCE_DIRECTORY/Default/"
+	cp -R $_CONFIGURATION_DIRECTORY/Default/Extensions "$_INSTANCE_DIRECTORY/Default/" 2>/dev/null
 
 	# automatically update configuration
 	_ESCAPED_INSTANCE_DIRECTORY=$(echo $_INSTANCE_DIRECTORY | sed -e "s/\//\\\\\//g")
-	$_CONF_RUN_AS_CMD mkdir -p $_INSTANCE_DIRECTORY/Downloads
+	mkdir -p $_INSTANCE_DIRECTORY/Downloads
 
-	$_CONF_RUN_AS_CMD $_CONF_GNU_SED -i "s/\"default_directory\":\".*\/Downloads\"}/\"default_directory\":\"${_ESCAPED_INSTANCE_DIRECTORY}\/Downloads\"}/" "$_NEW_INSTANCE_DIRECTORY/Default/Preferences"
+	$_CONF_GNU_SED -i "s/\"default_directory\":\".*\/Downloads\"}/\"default_directory\":\"${_ESCAPED_INSTANCE_DIRECTORY}\/Downloads\"}/" "$_NEW_INSTANCE_DIRECTORY/Default/Preferences"
 }
